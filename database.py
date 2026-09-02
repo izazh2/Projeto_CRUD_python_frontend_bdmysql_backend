68e93a6f5961bb76e2ca173e0d4d69761dd193f6
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-09-02 19:31:12 -0300

    diversas alterações

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost/bdpessoas"
+DATABASE_URL = "mysql+pymysql://root:root@localhost/bdpessoas02"
 
 engine = create_engine(DATABASE_URL)
 
